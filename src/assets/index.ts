07d3a2c07460a61999f994f1ffcaa86a21ec49c7
Grenish rai <ariestheracer@gmail.com>
2024-06-03 03:22:35 +0530

    Added logos

---
+++ src/assets/index.ts
@@ -0,0 +1,14 @@
+export { default as behance } from './behance.svg';
+export { default as codepen } from './codepen.svg';
+export { default as discord } from './discord.svg';
+export { default as dribbble } from './dribbble.svg';
+export { default as instagram } from './instagram.svg';
+export { default as linkedin } from './linkedin.svg';
+export { default as medium } from './medium.svg';
+export { default as pinterest } from './pinterest.svg';
+export { default as reddit } from './reddit.svg';
+export { default as stackOverflow } from './stack-overflow.svg';
+export { default as threads } from './threads.svg';
+export { default as tiktok } from './tiktok.svg';
+export { default as twitterX } from './twitter-x.svg';
+export { default as youtube } from './youtube.svg';
