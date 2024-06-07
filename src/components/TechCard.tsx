6226b92541ff56f4cf2e7bf683b164eee8335952
Grenish rai <ariestheracer@gmail.com>
2024-06-07 23:48:22 +0530

    Created a checkbox

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -15,12 +15,17 @@ const TechCard: React.FC<TechCardProps> = ({ imgSrc, altText, techName }) => {
         <img src={imgSrc} alt={altText} className="w-22 h-20" />
       </div>
       <span className="mt-2 mb-1 text-center">{techName}</span>
-      <button
+      <label
         className={`border-2 border-primary-200 active:scale-50 transition-all duration-200 ease-in-out ${
           isTechSelected ? "bg-accent-200" : ""
-        } w-full flex items-center justify-center rounded-md p-1`}
-        onClick={() => setIsTechSelected(!isTechSelected)}
+        } w-full flex items-center justify-center rounded-md p-1 cursor-pointer`}
       >
+        <input
+          type="checkbox"
+          checked={isTechSelected}
+          onChange={() => setIsTechSelected(!isTechSelected)}
+          className="hidden"
+        />
         {isTechSelected ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -42,7 +47,7 @@ const TechCard: React.FC<TechCardProps> = ({ imgSrc, altText, techName }) => {
             <path d="M224,128a8,8,0,0,1-8,8H136v80a8,8,0,0,1-16,0V136H40a8,8,0,0,1,0-16h80V40a8,8,0,0,1,16,0v80h80A8,8,0,0,1,224,128Z"></path>
           </svg>
         )}
-      </button>
+      </label>
     </div>
   );
 };
