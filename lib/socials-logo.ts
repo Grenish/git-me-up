e5602497107fd927ab4925e61855ea0f30cf1d96
Grenish rai <mrcoder2033d@gmail.com>
2025-10-20 22:07:57 +0530

    added social logo

---
+++ lib/socials-logo.ts
@@ -0,0 +1,63 @@
+export interface SocialLogosInterface {
+  iconName: string;
+  iconPath: string;
+}
+
+export const SocialLogos: SocialLogosInterface[] = [
+  {
+    iconName: "behance",
+    iconPath: "/socials/behance.svg",
+  },
+  {
+    iconName: "buymeacoffee",
+    iconPath: "/socials/buymeacoffee.svg",
+  },
+  {
+    iconName: "discord",
+    iconPath: "/socials/discord.svg",
+  },
+  {
+    iconName: "dribbble",
+    iconPath: "/socials/dribbble.svg",
+  },
+  {
+    iconName: "facebook",
+    iconPath: "/socials/facebook.svg",
+  },
+  {
+    iconName: "gmail",
+    iconPath: "/socials/gmail.svg",
+  },
+  {
+    iconName: "hashnode",
+    iconPath: "/socials/hashnode.svg",
+  },
+  {
+    iconName: "instagram",
+    iconPath: "/socials/instagram.svg",
+  },
+  {
+    iconName: "linkedin",
+    iconPath: "/socials/linkedin.svg",
+  },
+  {
+    iconName: "pinterest",
+    iconPath: "/socials/pinterest.svg",
+  },
+  {
+    iconName: "spotify",
+    iconPath: "/socials/spotify.svg",
+  },
+  {
+    iconName: "twitch",
+    iconPath: "/socials/twitch.svg",
+  },
+  {
+    iconName: "x",
+    iconPath: "/socials/x.svg",
+  },
+  {
+    iconName: "youtube",
+    iconPath: "/socials/youtube.svg",
+  },
+];
